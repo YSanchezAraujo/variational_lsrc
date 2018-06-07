12024b0284a933679fde25facec1ca0e93746fe8
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2018-06-07 12:07:45 -0400

    Update itod.py

--- itod.py
+++ itod.py
@@ -1,4 +1,8 @@
 import os
 from argparse import ArgumentParser
 from nipype import Function, Node, Workflow, IdentityInterface
-# TODO: everything
+# TODO: 
+#     1. collect bayes factor scores and either put it into one file to feed to R or
+#         collect it within R and have it as an R object
+#     2. run the brain to diagnosis step with the bayes factor as prior logodds
+#     3. save results
