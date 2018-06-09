1d4e542062b4a674ed9e91c79a69002c77e9a349
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2018-06-08 22:20:49 -0400

    Update itod_template.r

--- itod_template.r
+++ itod_template.r
@@ -4,3 +4,33 @@ library('h5')
 library('varbvs')
 
 args = commandArgs(trailingOnly=T)
+data_path = args[1]
+bf_path = args[2]
+save_path = args[3]
+
+df <- read.csv(bf_path)
+N <- dim(df)[1]
+
+bf <- rep(0., N)
+bflog10 <- rep(0., N)
+
+for (idx in 1:N) {
+    fp <- as.character(df[idx, 1])
+    fpdf <- read.csv(fp)
+    bf[idx] = fpdf[1,2]
+    bflog10[idx] = fpdf[2,2]
+}
+
+file <- h5file(data_path, 'r')
+X <- file["I"][,]
+Z <- file["Z"][,]
+y <- file["y"][,]
+
+fit <- varbvs(X, Z, y, family="binomial", logodds=matrix(bf, 1))
+
+dir.create("fxvb_out")
+setwd(file.path(getwd(), "fxvb_out"))
+
+# things to save together
+# logw, w, sa, logoods, pip, beta
+## eta, s, mu, alpha
