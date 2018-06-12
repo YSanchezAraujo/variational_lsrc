462b230b176e5260416d16a0097c0a9deaa92f90
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2018-06-12 16:21:16 -0400

    Update cmd_itod.sh

--- cmd_itod.sh
+++ cmd_itod.sh
@@ -1,5 +1,5 @@
 python itod.py -dp /storage/gablab001/data/genus/GIT/genus/bayes/data_sets/files_for_edward/brain_snp_covars_meancentered_scaled.h5 \
 -sn /storage/gablab001/data/genus/GIT/genus/bayes/data_sets/files_for_edward/dev_for_container/fxvb_out \
--ip /storage/gablab001/data/genus/GIT/genus/genus_img.sqsh 
+-ip /storage/gablab001/data/genus/GIT/genus/genus_img.sqsh \
 -bp /storage:/storage \
 -bf /storage/gablab001/data/genus/GIT/genus/bayes/data_sets/files_for_edward/dev_for_container/bf_out
