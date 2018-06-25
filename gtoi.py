b322881e9fa5d0df737614366b7f2183528435dd
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2018-06-25 15:40:06 -0400

    Update gtoi.py

--- gtoi.py
+++ gtoi.py
@@ -10,6 +10,7 @@ parser.add_argument("-nr", "--nruns", type=str, help="number of instances to spi
 parser.add_argument("-dp", "--data_path", type=str, help="where data is")
 parser.add_argument("-sn", "--save_name", type=str, help="name of results file")
 parser.add_argument("-cn", "--cnames", type=str, help="column string dataset in h5 file")
+parser.add_argument("-cv", "--cvpath", type=str, help="base path to save cv indices", required=False)
 args = parser.parse_args()
 
 try:
@@ -27,17 +28,18 @@ wf.base_dir = cwd
 Iternode = Node(IdentityInterface(fields=["run_idx"]), name="Iternode")
 Iternode.iterables = ("run_idx", np.arange(nruns)+1)
 
-def write_varbvs(base_dir, data_path, save_name, col_idx, cnames):
+def write_varbvs(base_dir, data_path, save_name, col_idx, cnames, cvpath):
     import os
     f_name = "bf-feature_idx-{}.csv".format(col_idx)
     shfile = "\n".join([
         "#!/bin/bash", ("R_DEFAULT_PACKAGES= Rscript "
-        "{script} {data_path} {save_name} {col_idx} {cnames}")
+        "{script} {data_path} {save_name} {col_idx} {cnames} {cvpath}")
     ])
     file_name = os.path.join(base_dir, "gtoi_template.r")
     r_file = shfile.format(
         script=file_name, data_path=data_path,
-        save_name=save_name, col_idx=col_idx, cnames=cnames
+        save_name=save_name, col_idx=col_idx,
+        cnames=cnames, cvpath=cvpath
     )
     if os.getcwd() != base_dir:
         os.chdir(base_dir)
@@ -70,7 +72,10 @@ def run_sing(bind_path, img_path, base_dir, run_idx, script_path):
     return None
 
 Write_Varbvs = Node(interface=Function(
-        input_names=["base_dir", "data_path", "save_name", "col_idx", "cnames"],
+        input_names=[
+            "base_dir", "data_path", "save_name",
+            "col_idx", "cnames", "cvpath"
+        ],
         output_names=["script_path", "col_idx", "save_name"],
         function=write_varbvs
     ),
@@ -89,6 +94,7 @@ Write_Varbvs.inputs.base_dir = wf.base_dir
 Write_Varbvs.inputs.data_path = args.data_path
 Write_Varbvs.inputs.save_name = args.save_name
 Write_Varbvs.inputs.cnames = args.cnames
+Write_Varbvs.inputs.cvpath = args.cvpath
 wf.connect(Iternode, "run_idx", Write_Varbvs, "col_idx")
 Run_Sing.inputs.bind_path = args.bind_path
 Run_Sing.inputs.img_path = args.img_path
