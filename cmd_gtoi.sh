ed3ab534750a709431200474153a724b981cc016
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2018-06-28 14:47:13 -0400

    Update cmd_gtoi.sh

--- cmd_gtoi.sh
+++ cmd_gtoi.sh
@@ -1,7 +1,8 @@
 python gtoi.py -dp /storage/gablab001/data/genus/GIT/genus/bayes/data_sets/files_for_edward/brain_snp_covars_meancentered_scaled.h5 \
--sn /storage/gablab001/data/genus/GIT/genus/bayes/data_sets/files_for_edward/dev_for_container/bf_out \
+-sn /storage/gablab001/data/genus/GIT/genus/bayes/data_sets/files_for_edward/dev_for_container/bf_out_cv \
 -nr 170 \
 -ip /storage/gablab001/data/genus/GIT/genus/genus_img.sqsh \
 -bp /storage:/storage \
 -cn I_cols \
--cv /storage/gablab001/data/genus/GIT/genus/bayes/data_sets/files_for_edward/dev_for_container
+-cv /storage/gablab001/data/genus/GIT/genus/bayes/data_sets/files_for_edward/dev_for_container/cv_splits.csv \
+-gd /storage/gablab001/data/genus/GIT/genus/bayes/data_sets/files_for_edward/dev_for_container/gtoi_res
