c815c469e927a6b0e9976246d23f0c62df8a5d3c
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2018-07-03 13:45:27 -0400

    Create itod_cv_template.r

---
+++ itod_cv_template.r
@@ -0,0 +1,42 @@
+#!/usr/bin Rscript
+
+library('h5')
+library('varbvs')
+
+args = commandArgs(trailingOnly=T)
+data_path = args[1]
+bf_path = args[2]
+save_path = args[3]
+cv_path = args[4]
+cv_idx = args[5]
+
+df <- read.csv(bf_path)
+test_idx <- read.csv(cv_path)[, paste("test", cv_idx, sep="")]
+N <- dim(df)[1]
+
+bf <- rep(0., N)
+bflog10 <- rep(0., N)
+
+for (idx in 1:N) {
+    fpdf <- read.csv(as.character(df[idx, 1]))
+    bf[idx] = fpdf[1,2]
+    bflog10[idx] = fpdf[2,2]
+}
+
+file <- h5file(data_path, 'r')
+X <- file["I"][test_idx, ]
+Z <- file["Z"][test_idx, ]
+y <- file["y"][test_idx]
+
+fit <- varbvs(X, Z, y, family="binomial", logodds=matrix(bflog10))
+
+dir.create(paste("fxvb_out_cv_", cv_idx, sep=""))
+setwd(file.path(getwd(), paste("fxvb_out_cv_", cv_idx, sep="")))
+
+n170 <- as.data.frame(cbind(fit$logw, fit$w, fit$sa, fit$logodds, fit$pip, fit$beta))
+colnames(n170) <- c("logw", "w", "sa", "logodds", "pip", "beta")
+write.csv(n170, "logw_w_sa_logodds_pip_beta.csv", row.names=F)
+write.csv(as.data.frame(fit$eta), "eta.csv", row.names=F)
+write.csv(as.data.frame(fit$s), "s.csv", row.names=F)
+write.csv(as.data.frame(fit$mu), "mu.csv", row.names=F)
+write.csv(as.data.frame(fit$alpha), "alpha.csv", row.names=F)
