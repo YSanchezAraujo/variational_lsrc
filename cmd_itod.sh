8e460504a934b0d38baeb00d6f63e630872fb79a
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2019-07-16 09:40:37 -0400

    Update cmd_itod.sh

--- cmd_itod.sh
+++ cmd_itod.sh
@@ -1,5 +1,7 @@
-python itod.py -dp /storage/gablab001/data/genus/GIT/genus/bayes/data_sets/files_for_edward/brain_snp_covars_meancentered_scaled.h5 \
--sn /storage/gablab001/data/genus/GIT/genus/bayes/data_sets/files_for_edward/dev_for_container/fxvb_out \
+python itod_cv.py -dp /storage/gablab001/data/genus/GIT/genus/bayes/data_sets/files_for_edward/brain_snp_covars_meancentered_scaled.h5 \
+-sn /storage/gablab001/data/genus/GIT/genus/bayes/data_sets/files_for_edward/dev_for_container/fxvb_out_cv \
 -ip /storage/gablab001/data/genus/GIT/genus/genus_img.sqsh \
 -bp /storage:/storage \
--bf /storage/gablab001/data/genus/GIT/genus/bayes/data_sets/files_for_edward/dev_for_container/bf_out
+-bf /storage/gablab001/data/genus/GIT/genus/bayes/data_sets/files_for_edward/dev_for_container/bf_out_cv \
+-nc 100 \
+-cp /storage/gablab001/data/genus/GIT/genus/bayes/data_sets/files_for_edward/dev_for_container/shuffle_split_cv.csv
